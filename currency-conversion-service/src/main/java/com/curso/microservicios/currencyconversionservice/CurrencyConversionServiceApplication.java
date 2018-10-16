f09c2b217304077dccfbb9f9a1af654b9518a525
Oscar <oscar.marbac@gmail.com>
2018-10-16 16:23:16 +0200

    fin practica

--- currency-conversion-service/src/main/java/com/curso/microservicios/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/curso/microservicios/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,13 +2,18 @@ package com.curso.microservicios.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableFeignClients("com.curso.microservicios.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
+	
 }
