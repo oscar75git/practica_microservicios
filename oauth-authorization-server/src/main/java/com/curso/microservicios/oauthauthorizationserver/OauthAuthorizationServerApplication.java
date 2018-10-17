c6653a5cafb594ff2b5ae885383a3eaf8b1847c3
Oscar <oscar.marbac@gmail.com>
2018-10-17 16:02:29 +0200

    fin practica

---
+++ oauth-authorization-server/src/main/java/com/curso/microservicios/oauthauthorizationserver/OauthAuthorizationServerApplication.java
@@ -0,0 +1,12 @@
+package com.curso.microservicios.oauthauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+	}
+}
