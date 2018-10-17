a69f77826478e1e8c3617a011c3491fd840576ef
Oscar <oscar.marbac@gmail.com>
2018-10-17 09:47:50 +0200

    inicio practica10

---
+++ practica-microservicios-root/src/main/java/com/curso/microservicios/practicamicroserviciosroot/PracticaMicroserviciosRootApplication.java
@@ -0,0 +1,12 @@
+package com.curso.microservicios.practicamicroserviciosroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaMicroserviciosRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaMicroserviciosRootApplication.class, args);
+	}
+}
